b9af1803fe6e60aa82b4dddfbb99aefd7cf23dbe
steve <steverios_sr@yahoo.com>
2024-04-28 12:56:41 -0500

    added button and keyboard logic

--- Calculator/src/App.vue
+++ Calculator/src/App.vue
@@ -1,30 +1,62 @@
 <script setup>
-import { ref } from 'vue';
+import { onBeforeUnmount, onMounted, ref } from 'vue';
 
-const userInput = ref('0');
+const userInput = ref('');
 const evaluateThis = ref([]);
 
 function calculate(op) {
-  if (evaluate.value) {
-    evaluateThis.value.push(op);
-    evaluateThis.value.push(userInput.value);
-  } else {
-    evaluateThis.value.push(userInput.value);
-  }
-  userInput.value = 0;
+  evaluateThis.value.push(userInput.value);
+  evaluateThis.value.push(op);
+  userInput.value = '';
 }
 
 function evaluate() {
-  console.log('calculate this!');
-  console.log(evaluateThis.value);
+  evaluateThis.value.push(userInput.value);
+  userInput.value = eval([...evaluateThis.value].join(''));
+  evaluateThis.value = [];
 }
+
+function clear() {
+  userInput.value = '';
+  evaluateThis.value = [];
+}
+
+function handleKeyPress(event) {
+  const key = event.key;
+
+  if (key >= '0' && key <= '9') {
+    userInput.value += key;
+  }
+
+  switch (key) {
+    case '+':
+    case '-':
+    case '*':
+    case '/':
+      calculate(key);
+      break;
+    case 'Enter':
+      evaluate();
+      break;
+    default:
+      break;
+  }
+}
+
+onMounted(() => {
+  window.addEventListener('keypress', handleKeyPress);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('keypress', handleKeyPress);
+});
 </script>
 
 <template>
   <div class="wrapper">
     <h1 class="sr-only">Calculator</h1>
-    <form @submit.prevent="evaluate">
-      <input v-model="userInput" type="number" />
+    <form @submit.prevent="evaluate()">
+      <input v-model="userInput" type="number" placeholder="0" />
       <div class="operators">
         <button @click="calculate('+')">+</button>
         <button @click="calculate('-')">-</button>
@@ -33,21 +65,22 @@ function evaluate() {
       </div>
       <div class="nums-and-stuff">
         <div class="nums">
-          <button>1</button>
-          <button>2</button>
-          <button>3</button>
-          <button>4</button>
-          <button>5</button>
-          <button>6</button>
-          <button>7</button>
-          <button>8</button>
-          <button>9</button>
+          <button type="button" @click="userInput += '1'">1</button>
+          <button type="button" @click="userInput += '2'">2</button>
+          <button type="button" @click="userInput += '3'">3</button>
+          <button type="button" @click="userInput += '4'">4</button>
+          <button type="button" @click="userInput += '5'">5</button>
+          <button type="button" @click="userInput += '6'">6</button>
+          <button type="button" @click="userInput += '7'">7</button>
+          <button type="button" @click="userInput += '8'">8</button>
+          <button type="button" @click="userInput += '9'">9</button>
         </div>
         <div class="stuff">
-          <button type="submit">C</button>
+          <button @click.prevent="clear()">C</button>
           <button type="submit">=</button>
         </div>
       </div>
+      <button type="button" @click="userInput += '0'">0</button>
     </form>
   </div>
 </template>
