23c7340fa74633b7e2ac906459abed9f211d5126
steve <steverios_sr@yahoo.com>
2024-06-06 19:09:52 -0500

    scaffolded vue project; removed boilerplate

--- rock-paper-scissors/vue/src/App.vue
+++ rock-paper-scissors/vue/src/App.vue
@@ -1,30 +1,33 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <main>
+    <header>
+      <h1>Rock Paper Scissors</h1>
+      <div>
+        <p>Score</p>
+        <p class="score"></p>
+      </div>
+    </header>
+  </main>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  & h1 {
+    padding: 0.5rem;
+  }
+
+  & div {
+    display: grid;
+    justify-content: center;
+    align-items: center;
+    margin: 0.5rem;
+    padding: 0.5rem;
+  }
 }
 </style>
