c2013c4f768818ea9200aa07f2fd401ef75ba2a9
steve <steverios_sr@yahoo.com>
2024-08-10 21:24:31 -0500

    adding game logic

--- rock-paper-scissors/react/src/App.tsx
+++ rock-paper-scissors/react/src/App.tsx
@@ -1,9 +1,56 @@
+import { useState } from 'react';
 import './App.css';
-import HandIcon from './components/HandIcon';
-import RockIcon from './components/RockIcon';
-import ScissorIcon from './components/ScissorIcon';
+import PlayerSection from './components/PlayerSection';
+
+const CHOICES = ['rock', 'paper', 'scissors'];
 
 export default function App() {
+  const [playerChoice, setPlayerChoice] = useState(CHOICES[0]);
+  const [computerChoice, setComputerChoice] = useState(CHOICES[0]);
+  const [result, setResult] = useState('');
+
+  function handlePlayerChoice(choice: string): void {
+    setPlayerChoice(choice);
+
+    const computerChoice = handleComputerChoice();
+
+    switch (choice) {
+      case 'rock':
+        if (computerChoice === 'rock') {
+          setResult('Tie');
+        } else if (computerChoice === 'paper') {
+          setResult('Computer wins');
+        } else if (computerChoice === 'scissors') {
+          setResult('Player wins');
+        }
+        break;
+      case 'paper':
+        if (computerChoice === 'rock') {
+          setResult('Player wins');
+        } else if (computerChoice === 'paper') {
+          setResult('Tie');
+        } else if (computerChoice === 'scissors') {
+          setResult('Computer wins');
+        }
+        break;
+      case 'scissors':
+        if (computerChoice === 'rock') {
+          setResult('Computer wins');
+        } else if (computerChoice === 'paper') {
+          setResult('Player wins');
+        } else if (computerChoice === 'scissors') {
+          setResult('Tie');
+        }
+        break;
+    }
+  }
+
+  function handleComputerChoice(): string {
+    const randomChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)];
+    setComputerChoice(randomChoice);
+    return randomChoice;
+  }
+
   return (
     <main>
       <header>
@@ -13,11 +60,7 @@ export default function App() {
           <p className="score"></p>
         </div>
       </header>
-      <section>
-        <HandIcon />
-        <RockIcon />
-        <ScissorIcon />
-      </section>
+      <PlayerSection choiceSetterFunc={handlePlayerChoice} />
     </main>
   );
 }
