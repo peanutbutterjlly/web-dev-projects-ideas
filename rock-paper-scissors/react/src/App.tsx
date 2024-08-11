d690a6e0ad21067c92d5d0fc3946ce20e6871fd2
steve <steverios_sr@yahoo.com>
2024-08-11 09:27:36 -0500

    refactor and style

--- rock-paper-scissors/react/src/App.tsx
+++ rock-paper-scissors/react/src/App.tsx
@@ -1,54 +1,38 @@
 import { useState } from 'react';
 import './App.css';
-import PlayerSection from './components/PlayerSection';
+import ChoiceButton from './components/ChoiceButton';
 
 const CHOICES = ['rock', 'paper', 'scissors'];
 
 export default function App() {
   const [playerChoice, setPlayerChoice] = useState(CHOICES[0]);
-  const [computerChoice, setComputerChoice] = useState(CHOICES[0]);
+  const [computerChoice, setComputerChoice] = useState('');
   const [result, setResult] = useState('');
 
-  function handlePlayerChoice(choice: string): void {
-    setPlayerChoice(choice);
-
-    const computerChoice = handleComputerChoice();
-
-    switch (choice) {
-      case 'rock':
-        if (computerChoice === 'rock') {
-          setResult('Tie');
-        } else if (computerChoice === 'paper') {
-          setResult('Computer wins');
-        } else if (computerChoice === 'scissors') {
-          setResult('Player wins');
-        }
-        break;
-      case 'paper':
-        if (computerChoice === 'rock') {
-          setResult('Player wins');
-        } else if (computerChoice === 'paper') {
-          setResult('Tie');
-        } else if (computerChoice === 'scissors') {
-          setResult('Computer wins');
-        }
-        break;
-      case 'scissors':
-        if (computerChoice === 'rock') {
-          setResult('Computer wins');
-        } else if (computerChoice === 'paper') {
-          setResult('Player wins');
-        } else if (computerChoice === 'scissors') {
-          setResult('Tie');
-        }
-        break;
-    }
+  function getRandomChoice(): string {
+    return CHOICES[Math.floor(Math.random() * CHOICES.length)];
   }
 
-  function handleComputerChoice(): string {
-    const randomChoice = CHOICES[Math.floor(Math.random() * CHOICES.length)];
-    setComputerChoice(randomChoice);
-    return randomChoice;
+  function determineWinner(player: string, computer: string): string {
+    if (player === computer) {
+      return `It's a Tie`;
+    }
+    if (
+      (player === 'rock' && computer === 'scissors') ||
+      (player === 'scissors' && computer === 'paper') ||
+      (player === 'paper' && computer === 'rock')
+    ) {
+      return 'You win!';
+    }
+
+    return 'Computer wins!';
+  }
+
+  function handlePlayerChoice(choice: string): void {
+    const computer = getRandomChoice();
+    setPlayerChoice(choice);
+    setComputerChoice(computer);
+    setResult(determineWinner(choice, computer));
   }
 
   return (
@@ -60,7 +44,25 @@ export default function App() {
           <p className="score"></p>
         </div>
       </header>
-      <PlayerSection choiceSetterFunc={handlePlayerChoice} />
+      <section className="result">
+        <h2>{result}</h2>
+      </section>
+      <div className="choices">
+        <section className="player">
+          <h2>Your Choice</h2>
+          {CHOICES.map((choice) => (
+            <ChoiceButton
+              key={choice}
+              choice={choice}
+              onClickFunc={() => handlePlayerChoice(choice)}
+            />
+          ))}
+        </section>
+        <section className="computer">
+          <h2>Computer Choice</h2>
+          {computerChoice && <ChoiceButton choice={computerChoice} />}
+        </section>
+      </div>
     </main>
   );
 }
---
+++ rock-paper-scissors/react/src/components/ChoiceButton.tsx
@@ -0,0 +1,33 @@
+import HandIcon from './HandIcon';
+import RockIcon from './RockIcon';
+import ScissorIcon from './ScissorIcon';
+
+/**
+ * A reusable button component for selecting a choice (rock, paper, or scissors).
+ *
+ * @param {string} choice - The type of choice to display (rock, paper, or scissors).
+ * @param {function} [onClickFunc] - An optional function to call when the button is clicked.
+ * @return {JSX.Element} A button element with the selected choice icon.
+ */
+export default function ChoiceButton({ choice, onClickFunc }) {
+  function Icon() {
+    switch (choice) {
+      case 'rock':
+        return <RockIcon />;
+      case 'paper':
+        return <HandIcon />;
+      case 'scissors':
+        return <ScissorIcon />;
+    }
+  }
+
+  if (!onClickFunc) {
+    return <Icon />;
+  }
+
+  return (
+    <button onClick={onClickFunc}>
+      <Icon />
+    </button>
+  );
+}
